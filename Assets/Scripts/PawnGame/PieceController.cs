d3c665d7e55692be6a54f747333560602f2fe228
Akira Watanabe <a0323w@gmail.com>
2022-07-10 18:58:35 +0900

    ugokanai...

--- Assets/Scripts/PawnGame/PieceController.cs
+++ Assets/Scripts/PawnGame/PieceController.cs
@@ -8,19 +8,15 @@ using UnityEngine;
 public class PieceController : MonoBehaviour
 {
     ///<summary>プレイヤー </summary>
-    public int Player;
+    public int _player;
     /// <summary> 駒の種類 </summary>
     public Type _type;
-    /// <summary> 何手経過したか </summary>
-    public int _turnCount;
-    /// <summary> 駒の配置されている位置 </summary>
-    public Vector2Int Pos, _oldPos;
     /// <summary> 移動状態 </summary>
     public List<Status> _status;
     /// <summary> 移動判定をとるためのフラグ </summary>
-    public bool _isSelect;
+    public bool _select;
     /// <summary> 移動時のマス選択 </summary>
-    RaycastHit _hitTile;
+    public RaycastHit _hitTile;
 
     //Queen = 5, Rook = 4, Bishop = 3, Knight = 2, Pawn = 1 と数字を振る
     public enum Type
@@ -44,7 +40,6 @@ public class PieceController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _turnCount = -1;
         _status = new List<Status>();
     }
 
@@ -55,36 +50,34 @@ public class PieceController : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 _mousePos = Input.mousePosition;
-            Ray _ray = Camera.main.ScreenPointToRay(_mousePos);
-            Debug.Log(_isSelect);
+            Vector3 _cameraPos = Camera.main.transform.position;
 
-            if (Physics.Raycast(_ray, out _hitTile, 50))
-              //Physics.Raycast(Rayの開始地点(ワールド座標), Rayの方向, Rayが衝突を検知する最大の距離)
+            if (Physics.Linecast(_cameraPos, _mousePos)) //ここ呼ばれてない...
             {
                 //Rayを飛ばし、タグが自分自身と同じだった場合選択状態の変更
                 if (_hitTile.collider.gameObject.tag == gameObject.tag)
                 {
-                    _isSelect = !_isSelect;
+                    _select = true;
                 }
                 //駒を選択中かつ、Rayがヒットしたオブジェクトがマスだった場合移動
-                if (_isSelect && _hitTile.collider.gameObject.tag == "Tile")
+                if (_select && _hitTile.collider.gameObject.tag == "Tile")
                 {
                     Vector3 _newPiecePos = _hitTile.collider.gameObject.transform.position;
                     transform.position = new Vector3(_newPiecePos.x, transform.position.y, _newPiecePos.z);
 
                     //移動後は駒の選択状態を非選択にする
-                    _isSelect = false;
+                    _select = false;
                 }
             }
             Debug.Log(_mousePos);
+            Debug.Log(_cameraPos);
         }
     }
 
     //初期設定
     public void SetPiece(int player, Type type)
     {
-        Player = player;
+        _player = player;
         _type = type;
-        _turnCount = -1; //初動に戻す
     }
 }
