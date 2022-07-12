5df0bc58502e7266ef9a5f32acdf6ee51c401c7f
Akira Watanabe <a0323w@gmail.com>
2022-07-12 16:57:00 +0900

    駒選択のフラグ反応←→移動×、NullReferenceException発生

--- Assets/Scripts/PawnGame/PieceController.cs
+++ Assets/Scripts/PawnGame/PieceController.cs
@@ -20,7 +20,7 @@ public class PieceController : MonoBehaviour
     /// <summary> Rayの衝突を確かめる </summary>
     public RaycastHit _hitTile;
     /// <summary> レイヤーマスク </summary>
-    private LayerMask _pieceLay;
+    private LayerMask _pieceLay = 6;
 
     //Queen = 5, Rook = 4, Bishop = 3, Knight = 2, Pawn = 1 と数字を振る
     public enum Type
@@ -42,29 +42,30 @@ public class PieceController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //マウス左クリックで駒を選び、二度目のクリックで配置場所を確定する
+        //マウス左クリックで駒を選び、二度目のクリックで配置場所を確定、移動する
         if (Input.GetMouseButtonDown(0))
         {
             //マウスの位置を取得し、Rayに代入
             Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Debug.DrawRay(_ray.origin, _ray.direction * 30, Color.green, 10, false); //Rayが黒番のカメラの方から出ている...Tag変えたらMainCameraの方に変わった
+            Debug.Log(_ray);
             /*↑ここまでは呼ばれている*/
 
             //マウスのポジションからRayを伸ばし、何かに当たったら_hitTileに代入する
             //↓このif文が呼ばれてない...どの引数がダメ？
-            if (Physics.Raycast( _ray /*投射対象のRay*/, out _hitTile /*衝突した相手オブジェクトの情報*/, 50 /*Rayの長さ(省略した場合、無限長)*/, _pieceLay /*衝突対象になるレイヤー(レイヤーマスク)*/ ))
-            {
+            //if (Physics.Raycast( _ray /*投射対象のRay*/, out _hitTile /*衝突した相手オブジェクトの情報*/, 50 /*Rayの長さ(省略した場合、無限長)*/, _pieceLay /*衝突対象になるレイヤー(レイヤーマスク)*/ ))
+            //{
+                _select = !_select; //「!」...現在の値とは反対の値を代入している(今回の場合、false→true)
                 Debug.Log("SelectPosition");
 
                 //駒を選択中、かつRayがヒットしたオブジェクトが盤面のマスならば移動する
-                if (_select && _hitTile.collider.gameObject.tag == "Tile")
+                if (_select/*(== true)*/ && _hitTile.collider.gameObject.tag == "Tile")//...NullReferenceException
                 {
-                    Vector3 _newPos = _hitTile.collider.gameObject.transform.position;
+                    Vector3 _newPos = Input.mousePosition;
                     transform.position = new Vector3(_newPos.x, _newPos.y, _newPos.z);
-
                     _select = false;
                 }
-            }
+            //}
         }
     }
 }
