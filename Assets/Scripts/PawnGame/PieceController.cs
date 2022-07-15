ca9da5affa0bddae15f17c8aeaad31d0ee4f8111
Akira Watanabe <a0323w@gmail.com>
2022-07-15 20:05:25 +0900

    駒を選択して移動する(駒ごとの動きはこれから)

--- Assets/Scripts/PawnGame/PieceController.cs
+++ Assets/Scripts/PawnGame/PieceController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 /// <summary> 
 /// 全ての駒に統一された動き
 /// </summary>
-public class PieceController : MonoBehaviour
+public class PieceController : MonoBehaviour, IPointerClickHandler
 {
     ///<summary> プレイヤー(白番) </summary>
     private const int _playerOne = 1;
@@ -17,10 +18,53 @@ public class PieceController : MonoBehaviour
     public Type _type;
     /// <summary> 移動判定をとるためのフラグ </summary>
     public bool _select;
+    /// <summary> Rayの長さ </summary>
+    [SerializeField] public float _rayDistance = 100;
     /// <summary> レイヤーマスク(Inspector内のLayerの番号) </summary>
     // ※レイヤーマスクの値は2bit値(2進数)で管理しているため、10進数で表示は×
-    private LayerMask _pieceLay = 1 << 8; //2進数で「1000」
-                                          //= LayerMask.NameToLayer(Layer名(string));
+    [SerializeField] LayerMask _tileLayer;
+    /// <summary> 駒を移動した時にcolliderの上に置く </summary>
+    [SerializeField] Vector3 _offset = Vector3.up;
+
+    Status _status = Status.Normal;
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print($"{ name } をクリックした");
+        ChangeState();
+    }
+
+    void ChangeState()
+    {
+        if (_status == Status.Normal)
+        {
+            _status = Status.Move;
+        }
+        else if (_status == Status.Move)
+        {
+            _status = Status.Normal;
+        }
+    }
+
+    bool Move()
+    {
+        //マウスの位置を取得し、Rayに代入
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(_ray.origin, _ray.direction * 30, Color.green, 30/*実行時間(秒)*/, false);
+        Debug.Log(_ray);
+
+        if (Physics.Raycast(_ray, out RaycastHit hit, _rayDistance, _tileLayer))
+        {
+            GameObject _target = hit.collider.gameObject;
+            print($"Ray は {_target.name} に当たった");
+            this.transform.position = _target.transform.position + _offset;
+            return true;
+        }
+
+        return false;
+    }
 
     void Start()
     {
@@ -30,40 +74,14 @@ public class PieceController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //一回目のマウス左クリックで駒を選び、二度目のクリックで配置場所を確定、移動する(予定)
-        //同じ行動で別の処理にできる?
+        //一回目のマウス左クリックで駒を選び、二度目のクリックで配置場所を確定、移動する
         if (Input.GetMouseButtonDown(0))
         {
-            //マウスの位置を取得し、Rayに代入
-            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //Rayの衝突を確かめる
-            RaycastHit _hit;
-            //駒を選択状態にする
-            _select = !_select; //false → true (_select = true;)
-
-            Debug.DrawRay(_ray.origin, _ray.direction * 30, Color.green, 30/*実行時間(秒)*/, false);
-            Debug.Log(_ray);
-
-            if (Physics.Raycast(_ray.origin, _ray.direction * 30, out _hit, Mathf.Infinity, _pieceLay))
+            if (_status == Status.Move)
             {
-                //  ↓選択中　↓白番　　　　　　　　　　　　　↓Rayが"WhitePiece"タグのオブジェクトに当たった時
-                if (_select && _currentPlayer == _playerOne && _hit.collider.gameObject.tag == "WhitePiece")
+                if (Move())
                 {
-                    Debug.Log("select");
-                    //↓の処理をすべての駒が行っているため、一ヶ所に駒が集まって衝突してしまう
-                    Vector3 _newPos = _hit.collider.gameObject.transform.position;
-                    transform.position = new Vector3(_newPos.x, _newPos.y, _newPos.z);
-                    _currentPlayer = _playerTwo; //手番の変更(白番→黒番)
-                    _select = false;
-                }
-                //       ↓選択中　↓黒番　　　　　　　　　　　　　↓Rayが"BlackPiece"タグのオブジェクトに当たった時
-                else if (_select && _currentPlayer == _playerTwo && _hit.collider.gameObject.tag == "BlackPiece")
-                {
-                    //↓の処理をすべての駒が行っているため、一ヶ所に駒が集まって衝突してしまう
-                    Vector3 _newPos = _hit.collider.gameObject.transform.position;
-                    transform.position = new Vector3(_newPos.x, _newPos.y, _newPos.z);
-                    _currentPlayer = _playerOne; //手番の変更(黒番→白番)
-                    _select = false;
+                    ChangeState();
                 }
             }
         }
