90578cdb04dc2463f95ac674127827b0365cdb24
Akira Watanabe <a0323w@gmail.com>
2022-07-16 10:29:55 +0900

    白番、黒番それぞれの目線(カメラ)から駒を選択、移動させる処理

--- Assets/Scripts/PawnGame/PieceController.cs
+++ Assets/Scripts/PawnGame/PieceController.cs
@@ -16,8 +16,6 @@ public class PieceController : MonoBehaviour, IPointerClickHandler
     private int _currentPlayer;
     /// <summary> 駒の種類 </summary>
     public Type _type;
-    /// <summary> 移動判定をとるためのフラグ </summary>
-    public bool _select;
     /// <summary> Rayの長さ </summary>
     [SerializeField] public float _rayDistance = 100;
     /// <summary> レイヤーマスク </summary>
@@ -28,10 +26,15 @@ public class PieceController : MonoBehaviour, IPointerClickHandler
     [SerializeField] Material _normalMaterial;
     [SerializeField] Material _moveMaterial;
     Renderer _renderer;
+    /// <summary> 黒番目線のカメラ </summary>
+    Camera _camera;
+    /// <summary> 駒の状態 </summary>
+    public Status _status = Status.Normal;
 
-    Status _status = Status.Normal;
-
-
+    /// <summary>
+    /// マウスクリックが行われた時の処理
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnPointerClick(PointerEventData eventData)
     {
         print($"{ name } をクリックした");
@@ -55,11 +58,21 @@ public class PieceController : MonoBehaviour, IPointerClickHandler
     bool Move()
     {
         //マウスの位置を取得し、Rayに代入
-        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition); //メインカメラ(白番目線)からRayをとばす
+        Ray _ray2 = _camera.ScreenPointToRay(Input.mousePosition);    //secondカメラ(黒番目線)からRayをとばす
 
+        //白番目線
         if (Physics.Raycast(_ray, out RaycastHit hit, _rayDistance, _tileLayer))
         {
             GameObject _target = hit.collider.gameObject;
+            print($"Ray は {_target.name} に当たった");                     //「 print($"..."); 」= Debug.Log("..."); と同じ
+            this.transform.position = _target.transform.position + _offset;
+            return true;
+        }
+        //黒番目線
+        else if (Physics.Raycast(_ray2, out RaycastHit hit2, _rayDistance, _tileLayer))
+        {
+            GameObject _target = hit2.collider.gameObject;
             print($"Ray は {_target.name} に当たった");
             this.transform.position = _target.transform.position + _offset;
             return true;
@@ -70,14 +83,16 @@ public class PieceController : MonoBehaviour, IPointerClickHandler
 
     void Start()
     {
-        _currentPlayer = _playerOne;
-        _renderer = GetComponent<Renderer>();
+        _currentPlayer = _playerOne;                                       //最初は白番から
+        _camera = GameObject.Find("Camera(black)").GetComponent<Camera>(); //黒番目線のカメラを見つけてくる
+        _renderer = GetComponent<Renderer>();                              //駒のRenderer(コンポーネント)をとってくる
     }
 
     // Update is called once per frame
     void Update()
     {
-        //一回目のマウス左クリックで駒を選び、二度目のクリックで配置場所を確定、移動する
+        //一回目のマウスクリックで駒を選び、二度目のクリックで配置場所を確定、移動する
+        //マウスクリックの中でも、左クリックが行われた場合に以下の処理を行う
         if (Input.GetMouseButtonDown(0))
         {
             if (_status == Status.Move)
